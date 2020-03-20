56637c81448765cbb05d65bbfbb318f63e7608cf
Shiroki <tomatroom@qq.com>
2020-03-20 17:00:09 +0800

    添加A+B模式实现的随机时间功能

---
+++ ScfOperate.py
@@ -0,0 +1,142 @@
+import os
+import random
+import time
+import json
+from tencentcloud.common import credential
+from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
+from tencentcloud.scf.v20180416 import scf_client, models
+
+# 定义关于腾讯云的变量
+# 规范：全部单词首字母大写，中间无连接符号
+global random_time_lowerlimit, random_time_toplimit, SecretId, SecretKey, Region
+# 下次触发前的随机时间的范围:
+random_time_lowerlimit = 150
+random_time_toplimit = 300
+
+# 读取环境变量中的腾讯云api
+SecretId = ""
+SecretKey = ""
+
+# 读取环境变量中的腾讯云地域
+Region = ""
+
+
+# 写入环境变量（主要是为了更新refresh_token供下次使用）
+
+
+def EnvWrite(FunctionName, Namespace, ms_id, ms_secret, refresh_token, Another=None):
+    global SecretId, SecretKey, Region
+    try:
+        cred = credential.Credential(SecretId, SecretKey)
+        client = scf_client.ScfClient(cred, Region)
+    except TencentCloudSDKException as err:
+        print(err)
+    req = models.UpdateFunctionConfigurationRequest()
+    params = '{"FunctionName":"' + FunctionName + '",'
+    params += '"Namespace":"' + Namespace + '",'
+    params += '"Environment":{"Variables":['
+    params += '{"Key":"Region","Value":"' + Region + '"},'
+    params += '{"Key":"refresh_token","Value":"' + refresh_token + '"},'
+    params += '{"Key":"ms_id","Value":"' + ms_id + '"},'
+    params += '{"Key":"ms_secret","Value":"' + ms_secret + '"},'
+    params += '{"Key":"SecretId","Value":"' + SecretId + '"},'
+    params += '{"Key":"SecretKey","Value":"' + SecretKey + '"},'
+    if Another:
+        for key in Another:
+            if Another[key]:
+                params += '{"Key":"'+ key + '","Value":"' + Another[key] + '"},'
+    params = params.rstrip(",")
+    params += ']}}'
+    #print(params)
+    req.from_json_string(params)
+    try:
+        # 以下照抄腾讯云sdk例程
+        resp = client.UpdateFunctionConfiguration(req)
+        print(resp.to_json_string())
+    except TencentCloudSDKException as err:
+        print(err)
+    return
+
+
+# 用于AB结构下的触发器更新
+def ABTrigUpdate(AnotherFunctionName, AnotherRegion, AnotherNamespace, TriggerName):
+    global SecretId, SecretKey, Region
+    try:
+        cred = credential.Credential(SecretId, SecretKey)
+        client = scf_client.ScfClient(cred, AnotherRegion)
+    except TencentCloudSDKException as err:
+        print(err)
+    # 删除旧触发器
+    try:
+        req = models.DeleteTriggerRequest()
+        params = '{"FunctionName":"' + AnotherFunctionName + '",'
+        params += '"TriggerName":"' + TriggerName + '",'
+        params += '"Namespace":"' + AnotherNamespace + '",'
+        params += '"Type":"timer"}'
+        req.from_json_string(params)
+        resp = client.DeleteTrigger(req)
+        print(resp.to_json_string())
+    except TencentCloudSDKException as err:
+        print(err)
+
+    # 添加新触发器
+    try:
+        next_time = time.time()
+        next_time += random.randint(random_time_lowerlimit,
+                                    random_time_toplimit)
+
+        req = models.CreateTriggerRequest()
+        params = '{"FunctionName":"' + AnotherFunctionName + '",'
+        params += '"TriggerName":"' + TriggerName + '",'
+        params += '"Namespace":"' + AnotherNamespace + '",'
+        params += '"Type":"timer",'
+        params += '"TriggerDesc":"' + \
+            to_china_timezone_cron(next_time) + '"}'
+        req.from_json_string(params)
+        resp = client.CreateTrigger(req)
+        print(resp.to_json_string())
+    except TencentCloudSDKException as err:
+        print(err)
+    return
+
+
+# 用于主从结构下的触发器更新
+def MSTrigUpdate_Master(AnotherFunctionName, AnotherRegion, AnotherNamespace, TriggerName, FunctionName, Namespace):
+    global SecretId, SecretKey, Region
+    try:
+        cred = credential.Credential(SecretId, SecretKey)
+        client = scf_client.ScfClient(cred, AnotherRegion)
+    except TencentCloudSDKException as err:
+        print(err)
+    # 启动从函数
+    pass_params = {
+        "FunctionName": FunctionName,
+        "Region": Region,
+        "Namespace": Namespace,
+        "TriggerName": TriggerName,
+        "SecretId": SecretId,
+        "SecretKey": SecretKey
+    }
+    pass_params_str = json.dumps(pass_params)
+    req = models.InvokeRequest()
+    params = '{"FunctionName":"' + AnotherFunctionName + '",'
+    params += '"InvocationType":"Event",'
+    params += '"ClientContext":"' + pass_params_str + '",'
+    params += '"Namespace":"' + AnotherNamespace + '"}'
+    req.from_json_string(params)
+    return
+
+
+def MSTrigUpdate_Slave(event, content):
+    FunctionName = content["FunctionName"]
+    Region = content["Region"]
+    Namespace = content["Namespace"]
+    TriggerName = content["TriggerName"]
+    SecretId = content["SecretId"]
+    SecretKey = content["SecretKey"]
+    ABTrigUpdate(FunctionName, Region,
+                 Namespace, TriggerName)
+    return
+
+def to_china_timezone_cron(timestamp): return time.strftime(
+    "%S %M %H %d %m * %Y", time.gmtime(timestamp+8*60*60))
--- index.py
+++ index.py
@@ -1,25 +1,34 @@
 import requests as req
 import json
 import os
-from tencentcloud.common import credential
-from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
-from tencentcloud.scf.v20180416 import scf_client, models
+import ScfOperate
+
+
+# 获取环境变量用的字符串是一大坑点！
+# 为什么Python没有nameof方法？？
+# （然而如果一开始就规范好变量命名规则就没那么多事了
 
 # 定义关于Azure AD的变量
 # 规范：使用全小写，中间用下划线连接
+
+# 读取环境变量中的微软应用api
 ms_id = ""
 ms_secret = ""
+
+# 读取环境变量中的refresh_token
 refresh_token = ""
 
 # 定义关于腾讯云的变量
 # 规范：全部单词首字母大写，中间无连接符号
-SecretId = ""
-SecretKey = ""
-
 FunctionName = ""
 Namespace = ""
-Region = ""
-
+# 下面是递归触发相关
+TrigUpdateType = ""
+AnotherFunctionName = ""
+AnotherRegion = ""
+AnotherNamespace = ""
+TriggerName = ""
+TriggerName_Default = "RandomTimer"
 
 # 微软api列表，直接复制粘贴自原脚本
 api_list = [
@@ -36,40 +45,13 @@ api_list = [
 ]
 
 
-# 写入环境变量（主要是为了更新refresh_token供下次使用）
-def envwrite():
-    req = models.UpdateFunctionConfigurationRequest()
-    params = '{"FunctionName":"' + FunctionName + '",'
-    params += '"Environment":{"Variables":['
-    params += '{"Key":"Region","Value":"' + Region + '"},'
-    params += '{"Key":"refresh_token","Value":"' + refresh_token + '"},'
-    params += '{"Key":"ms_id","Value":"' + ms_id + '"},'
-    params += '{"Key":"ms_secret","Value":"' + ms_secret + '"},'
-    params += '{"Key":"SecretId","Value":"' + SecretId + '"},'
-    params += '{"Key":"SecretKey","Value":"' + SecretKey + '"}]}}'
-    req.from_json_string(params)
-    print(req)
-    try:
-        # 以下照抄腾讯云sdk例程
-        cred = credential.Credential(SecretId, SecretKey)
-        client = scf_client.ScfClient(cred, Region)
-        resp = client.UpdateFunctionConfiguration(req)
-
-        print(resp.to_json_string())
-
-    except TencentCloudSDKException as err:
-        print(err)
-    pass
+def get_api(url, headers):
+    return req.get(url, headers=headers).status_code == 200
 
 
-def getapi(url, headers):
-    if req.get(url, headers=headers).status_code == 200:
-        print("调用"+url+"成功")
-
-
-# 拿到最新的access_token，顺便更新环境变量里的refresh_token
-def gettoken():
-    global refresh_token
+# 拿到最新的access_token
+def get_token():
+    global refresh_token, ms_id, ms_secret
     headers = {"Content-Type": "application/x-www-form-urlencoded"
                }
     data = {"grant_type": "refresh_token",
@@ -83,44 +65,93 @@ def gettoken():
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt["refresh_token"]
     access_token = jsontxt["access_token"]
-    envwrite()
     return access_token
 
 
 def exe(event, content):
     # 全局变量要手动指定才能修改……
-    global FunctionName, Namespace, ms_id, ms_secret, SecretId, SecretKey, Region, refresh_token
+    global FunctionName, Namespace
 
-    # 从api入参处读取
-    FunctionName = content["function_name"]
-    Namespace = content["namespace"]
-
-    # 获取环境变量用的字符串是一大坑点！
-    # 为什么Python没有nameof方法？？
-    # （然而如果一开始就规范好变量命名规则就没那么多事了
+    global ms_id, ms_secret, refresh_token
 
     # 读取环境变量中的微软应用api
     ms_id = os.environ.get("ms_id")
     ms_secret = os.environ.get("ms_secret")
 
-    # 读取环境变量中的腾讯云api
-    SecretId = os.environ.get("SecretId")
-    SecretKey = os.environ.get("SecretKey")
-
-    # 读取环境变量中的腾讯云地域
-    Region = os.environ.get("Region")
-
     # 读取环境变量中的refresh_token
     refresh_token = os.environ.get("refresh_token")
 
-    access_token = gettoken()
+    ScfOperate.SecretId = os.environ.get("SecretId")
+    ScfOperate.SecretKey = os.environ.get("SecretKey")
+    ScfOperate.Region = os.environ.get("Region")
+
+    # 从api入参处读取
+    FunctionName = content["function_name"]
+    Namespace = content["namespace"]
+
+    access_token = get_token()
     headers = {
         "Authorization": access_token,
         "Content-Type": "application/json"
     }
     try:
         for api in api_list:
-            getapi(api, headers)
+            get_api(api, headers)
     except:
-        print("pass")
-        pass
+        print("出现错误，跳过剩余")
+    print("API触发结束")
+    # 设定下一个触发时间
+    # ScfOperate.TrigUpdate(FunctionName, Namespace, TriggerName)
+
+    # 经过确认，运行状态下的云函数的触发器不能被修改
+    # 因此在云函数系统更新前，只能使用双函数递归调用实现随机时间
+    # 有AB、主从两种方案，干脆都写了
+    global TrigUpdateType
+    TrigUpdateType = os.environ.get("TrigUpdateType")
+    if not TrigUpdateType:
+        print("TrigUpdateType为空，将不会尝试设置随机时间")
+        ScfOperate.EnvWrite(FunctionName, Namespace, ms_id,
+                        ms_secret, refresh_token)
+        return
+    global AnotherFunctionName, AnotherRegion, AnotherNamespace, TriggerName
+
+    AnotherFunctionName = os.environ.get("AnotherFunctionName")
+    if not AnotherFunctionName:
+        print("AnotherFunctionName为空，将不会尝试设置随机时间")
+        return
+    
+    AnotherRegion = os.environ.get("AnotherRegion")
+    if not AnotherRegion:
+        print("AnotherRegion为空，将会默认为相同")
+        AnotherRegion = os.environ.get("Region")
+    
+    AnotherNamespace = os.environ.get("AnotherNamespace")
+    if not AnotherNamespace:
+        print("AnotherNamespace为空，将会默认为相同")
+        AnotherNamespace = Namespace
+
+    TriggerName = os.environ.get("AnotherTriggerName")
+    if not TriggerName:
+        print("TriggerName为空，将会使用默认值" + TriggerName_Default)
+        TriggerName = TriggerName_Default
+    
+    # 类型判断
+    if TrigUpdateType == "AB":
+        print("TrigUpdateType为A+B型")
+        ScfOperate.ABTrigUpdate(AnotherFunctionName,
+                                AnotherRegion, AnotherNamespace, TriggerName)
+    elif TrigUpdateType == "MS":
+        print("TrigUpdateType为M+S型")
+        ScfOperate.MSTrigUpdate_Master(AnotherFunctionName, AnotherRegion,
+            AnotherNamespace, TriggerName, FunctionName, Namespace)
+    Another = {
+        "TrigUpdateType": TrigUpdateType,
+        "AnotherFunctionName": AnotherFunctionName,
+        "AnotherRegion": os.environ.get("AnotherRegion"),
+        "AnotherNamespace": os.environ.get("AnotherNamespace"),
+        "TriggerName": os.environ.get("TriggerName"),
+    }
+    #print(Another)
+    ScfOperate.EnvWrite(FunctionName, Namespace, ms_id,
+                        ms_secret, refresh_token, Another)
+    return
